7e06a9f99cf4a4ee0c71cc676502024975a7ccf5
saibanandaM <Techsophy@143>
2023-03-21 11:54:41 +0530

    ui

--- base/actionspage.py
+++ base/actionspage.py
@@ -3,9 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+from utilities.customlogger import custom_logger
 
 
 class ActionPage:
+    log = custom_logger()
+
 
     def __init__(self, driver):
         self.driver = driver
@@ -19,12 +22,15 @@ class ActionPage:
 
         if str(locatortype) == "XPATH":
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(xpath)).click()
+            self.log.info("Click on the element ::"+ locatorvalue)
 
         elif str(locatortype) == "ID":
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(id)).click()
+            self.log.info("Click on the element ::"+ locatorvalue)
 
         elif str(locatortype) == "NAME":
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(name)).click()
+            self.log.info("Click on the element ::"+ locatorvalue)
 
     def sendkeys(self, locatorvalue, locatortype, value):
         xpath = (By.XPATH, locatorvalue)
@@ -33,9 +39,11 @@ class ActionPage:
 
         if str(locatortype) == "XPATH":
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(xpath)).send_keys(value)
+            self.log.info("sent key as  ::  " + value)
 
         elif str(locatortype) == "ID":
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(id)).send_keys(value)
+            self.log.info("sent key as  ::  " + value)
 
 
         elif str(locatortype) == "NAME":
---
+++ utilities/customlogger.py
@@ -0,0 +1,19 @@
+import logging
+import time
+import inspect
+
+def custom_logger():
+    logName = inspect.stack()[1][3]  # used to get the class/methode name from where logger methode called
+    logger = logging.getLogger(logName)  # creating logging object to pass the logname
+    logger.setLevel(logging.DEBUG)  # set log level
+    fileHandler = logging.FileHandler(
+        '/home/saibananda/PycharmProjects/Amazon_in/logs/' + time.strftime("%d-%m-%y") + '.text',
+        mode='a')  # create file handler to save the file
+    fileHandler.setLevel(logging.DEBUG)  # set log level to file handler
+    # create logging formate
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s",
+                                  datefmt='%d/%m/%y %I:%M:%S %p %A')
+    fileHandler.setFormatter(formatter)  # set the formater to filehandler
+    logger.addHandler(fileHandler)  # add file handler to logging
+
+    return logger  # retun logging object
